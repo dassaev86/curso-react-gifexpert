e0cfa3aab396c7f6715a68e7f442da1852698bde
Dassaev Novela <dassaev86@gmail.com>
2020-07-08 18:07:16 -0500

    Docs agregado

---
+++ docs/precache-manifest.7ce88f2b71183e2550064e9447dfa350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf1407a32331303968df44a084e5240",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24d0da3f1128edc6f7a3",
+    "url": "/static/css/main.a4ca3ba7.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24d0da3f1128edc6f7a3",
+    "url": "/static/js/main.bad9e8ce.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
